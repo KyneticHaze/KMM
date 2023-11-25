480ba3012312ea28c19f433a02f7278d35c3ba2e
Furkan Harmancı <rocklee34@hotmail.com>
2023-11-25 16:33:28 +0300

    feat: use_cases added :sparkles:

---
+++ shared/src/commonMain/kotlin/com/example/movie_kmm/domain/use_case/GetMovieByIdUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.movie_kmm.domain.use_case
+
+import com.example.movie_kmm.domain.model.Movie
+import com.example.movie_kmm.domain.repository.MovieRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class GetMovieByIdUseCase: KoinComponent {
+
+    private val repository: MovieRepository by inject()
+    @Throws(Exception::class)
+    suspend operator fun invoke(id: Int): Movie {
+        return repository.getMovieById(id)
+    }
+}
