61639513059e3300ea2ae594f829102fc95698dc
Furkan Harmancı <rocklee34@hotmail.com>
2023-11-25 16:33:49 +0300

    feat: koin started from ios :sparkles:

---
+++ shared/src/iosMain/kotlin/com/example/movie_kmm/util/KoinHelper.kt
@@ -0,0 +1,10 @@
+package com.example.movie_kmm.util
+
+import com.example.movie_kmm.di.getSharedModules
+import org.koin.core.context.startKoin
+
+fun initKoin() {
+    startKoin {
+        modules(getSharedModules())
+    }
+}
