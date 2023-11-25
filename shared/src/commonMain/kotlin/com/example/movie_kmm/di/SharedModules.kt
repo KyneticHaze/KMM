1768c57cf40afde9681c9829d3302348a44229bd
Furkan Harmancı <rocklee34@hotmail.com>
2023-11-25 16:31:57 +0300

    feat: koin di added :sparkles:

---
+++ shared/src/commonMain/kotlin/com/example/movie_kmm/di/SharedModules.kt
@@ -0,0 +1,41 @@
+package com.example.movie_kmm.di
+
+import com.example.movie_kmm.data.remote.MovieService
+import com.example.movie_kmm.data.remote.RemoteDataSource
+import com.example.movie_kmm.data.repository.MovieRepositoryImpl
+import com.example.movie_kmm.domain.repository.MovieRepository
+import com.example.movie_kmm.domain.use_case.GetMovieByIdUseCase
+import com.example.movie_kmm.domain.use_case.GetMoviesUseCase
+import com.example.movie_kmm.util.provideDispatcher
+import org.koin.dsl.module
+
+private val dataModule = module {
+    factory {
+        RemoteDataSource(get(), get())
+    }
+    factory {
+        MovieService()
+    }
+}
+
+private val utilModule = module {
+    factory {
+        provideDispatcher()
+    }
+}
+
+private val domainModule = module {
+    factory {
+        GetMoviesUseCase()
+    }
+    factory {
+        GetMovieByIdUseCase()
+    }
+    single<MovieRepository> {
+        MovieRepositoryImpl(get())
+    }
+}
+
+private val sharedModules = listOf(dataModule, utilModule, domainModule)
+
+fun getSharedModules() = sharedModules
