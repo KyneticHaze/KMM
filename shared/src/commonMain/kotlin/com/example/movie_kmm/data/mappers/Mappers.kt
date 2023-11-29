a051f21af13014f45fa0f10e4d622138dde2b8ab
Furkan Harmancı <rocklee34@hotmail.com>
2023-11-29 16:53:44 +0300

    feat: func wrote from MovieDTO to Movies :sparkles:

--- shared/src/commonMain/kotlin/com/example/movie_kmm/data/mappers/Mappers.kt
+++ shared/src/commonMain/kotlin/com/example/movie_kmm/data/mappers/Mappers.kt
@@ -1,16 +1,26 @@
 package com.example.movie_kmm.data.mappers
 
-import com.example.movie_kmm.data.remote.MovieRemote
+import com.example.movie_kmm.data.remote.dto.MovieDTO
+import com.example.movie_kmm.data.remote.dto.Result
 import com.example.movie_kmm.domain.model.Movie
+import com.example.movie_kmm.domain.model.Movies
 
-internal fun MovieRemote.toMovie(): Movie {
+internal fun Result.toMovie(): Movie {
     return Movie(
         id = id,
         title = title,
         description = overview,
-        imageUrl = getImageUrl(posterImage),
+        imageUrl = getImageUrl(posterPath),
         releaseDate = releaseDate
     )
 }
 
-private fun getImageUrl(posterImage: String) = "https://www.themoviedb.org/t/p/w500/${posterImage}"
+private fun getImageUrl(posterPath: String) = "https://www.themoviedb.org/t/p/w500/${posterPath}"
+
+
+internal fun MovieDTO.movieWithPage(): Movies {
+    return Movies(
+        page = page,
+        movies = results.map { it.toMovie() }
+    )
+}
