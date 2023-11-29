8644eda3777ee602638a6ce8b8e3a1fb6a8a6e39
Furkan Harmancı <rocklee34@hotmail.com>
2023-11-29 16:50:45 +0300

    refactor: func return type changed :package:

--- shared/src/commonMain/kotlin/com/example/movie_kmm/data/repository/MovieRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/example/movie_kmm/data/repository/MovieRepositoryImpl.kt
@@ -1,15 +1,18 @@
 package com.example.movie_kmm.data.repository
 
+import com.example.movie_kmm.data.mappers.movieWithPage
 import com.example.movie_kmm.data.mappers.toMovie
-import com.example.movie_kmm.data.remote.RemoteDataSource
+import com.example.movie_kmm.data.data_source.RemoteDataSource
 import com.example.movie_kmm.domain.model.Movie
+import com.example.movie_kmm.domain.model.Movies
 import com.example.movie_kmm.domain.repository.MovieRepository
+import io.ktor.util.logging.Logger
 
 internal class MovieRepositoryImpl(
     private val remoteDataSource: RemoteDataSource
 ): MovieRepository {
-    override suspend fun getMovies(page: Int): List<Movie> {
-        return remoteDataSource.getMovies(page).results.map { it.toMovie() }
+    override suspend fun getMovies(page: Int): Movies {
+        return remoteDataSource.getMovies(page).movieWithPage()
     }
 
     override suspend fun getMovieById(id: Int): Movie {
